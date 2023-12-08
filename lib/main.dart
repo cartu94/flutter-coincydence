d878dd2ecc7c59fd3e42b8c23ecb7cc01b34b1a4
Cartu <l.carturan@gmail.com>
2023-12-08 02:01:59 +0100

    main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import '/pages/drink_list.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Coincydence',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            brightness: Brightness.dark, seedColor: Colors.blueGrey),
         useMaterial3: true,
       ),
       home: const MyHomePage(),
@@ -32,17 +33,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Cocktail Book'),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       ),
       body: const Center(
-        child: 
-          DrinkList(),
-      )
+        child: DrinkList(),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
     );
   }
 }
--- lib/pages/drink_list.dart
+++ lib/pages/drink_list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:multi_dropdown/multiselect_dropdown.dart';
 
 import '/models/api.dart';
+import '/models/drinks.dart';
 
 class DrinkList extends StatefulWidget {
   const DrinkList({super.key});
@@ -10,29 +12,88 @@ class DrinkList extends StatefulWidget {
 }
 
 class _DrinkListState extends State<DrinkList> {
+  final List<String> ingredients = [
+    'Dry_Vermouth',
+    'Gin',
+    'Vodka',
+    'Tequila',
+    'White_Rum'
+  ];
+  List<String> selectedIngredients = [];
+  late Future<Drinks> drinkList;
+
+  @override
+  void initState() {
+    super.initState();
+    ingredients.sort();
+    drinkList = fetchAll();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
+        Container(
+          color: Theme.of(context).colorScheme.primaryContainer,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MultiSelectDropDown(
+              onOptionSelected: (value) {
+                setState(() {
+                  selectedIngredients =
+                      value.map((e) => e.value.toString()).toList();
+                  drinkList = selectedIngredients.isEmpty
+                      ? fetchAll()
+                      : fetchByMultipleIngredients(selectedIngredients);
+                });
+              },
+              options: ingredients
+                  .map((e) =>
+                      ValueItem(label: e.split('_').join(' '), value: e))
+                  .toList(),
+              selectionType: SelectionType.multi,
+              chipConfig: const ChipConfig(wrapType: WrapType.wrap),
+              selectedOptionIcon: const Icon(Icons.check),
+              dropdownBorderRadius: 10.0,
+              showClearIcon: true,
+              dropdownMargin: 5.0,
+            ),
+          ),
+        ),
+        
         Expanded(
           child: FutureBuilder(
-            future: fetchByMultipleIngredients(['Dry_Vermouth']),
+            future: drinkList,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
                 return ListView.builder(
                   itemCount: snapshot.data!.drinks.length,
                   itemBuilder: (context, index) {
                     return Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      padding: const EdgeInsets.symmetric(vertical: 3.0),
                       child: Row(
                         children: [
                           Expanded(
-                            child: ListTile(
-                              key: Key(snapshot.data!.drinks[index].id),
-                              title: Text(snapshot.data!.drinks[index].drink),
-                              leading: Image.network(
-                                  '${snapshot.data!.drinks[index].drinkThumb}/preview'),
+                            child: Card(
+                              elevation: 2,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              child: ListTile(
+                                key: Key(snapshot.data!.drinks[index].id),
+                                title: Text(snapshot.data!.drinks[index].drink),
+                                leading: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  child: Image.network(
+                                    '${snapshot.data!.drinks[index].drinkThumb}/preview',
+                                    cacheHeight: 100,
+                                    cacheWidth: 100,
+                                  ),
+                                ),
+                                contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 10.0, horizontal: 20.0),
+                              ),
                             ),
                           ),
                         ],
@@ -46,7 +107,7 @@ class _DrinkListState extends State<DrinkList> {
               return const CircularProgressIndicator();
             },
           ),
-        )
+        ),
       ],
     );
   }
